558afeab99cdf940c561d70eb50421bb25808cfe
tami0583211208@gmail.com <tami0583211208@gmail.com>
2022-07-27 13:03:46 +0300

    startedManegerGet

--- src/admin/services.js
+++ src/admin/services.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
-
-export default async function Services() {
-    try{
-    return await axios.get('https://meetings-test.herokuapp.com/user/ ')
-}
+export default function Services() {
+    return (
+      <main style={{ padding: "1rem 0" }}>
+        <h2>you are on the Services!!!!!!!!!!!!</h2>
+      </main>
+    );
+  }
+  export default async function get() {
+    try {
+        return await axios.get('https://meetings-test.herokuapp.com/service/')
+    }
     catch (error) {
         console.log('error in add user');
     }
-  }
-
-
-    
-
-
+}
